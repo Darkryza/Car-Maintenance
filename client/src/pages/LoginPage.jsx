86576a7de930303977c8e0f402a5b21628d0005a
Darkryza <82858316+Darkryza@users.noreply.github.com>
2025-12-14 23:31:02 +0800

    update

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,15 +1,53 @@
-import React from "react";
+import { useState } from "react";
 import "./LoginPage.css";
+import axios from "axios";
 
 function LoginPage() {
+  const [login, setLogin] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setLogin((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5484/login", login)
+      .then((res) => console.log(res.data))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="LoginPage">
-      <div className="login-container">
+      <form className="login-container" onSubmit={handleSubmit}>
         <label htmlFor="username">Username</label>
-        <input type="text" />
+        <input
+          type="text"
+          name="username"
+          className="login-input"
+          onChange={handleChange}
+          placeholder="Username"
+          value={login.username}
+        />
         <label htmlFor="password">Password</label>
-        <input type="password" />
-      </div>
+        <input
+          type="password"
+          name="password"
+          className="login-input"
+          onChange={handleChange}
+          placeholder="Password"
+          value={login.password}
+        />
+        <button className="login-btn">Submit</button>
+      </form>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import mysql from "mysql2";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
 const port = process.env.PORT || 5484;
 
 const conn = mysql.createConnection({
@@ -19,10 +23,13 @@ conn.connect((err) => {
     console.log(`DB Connection failed: ${err}`);
     return;
   }
+  console.log("DB connected");
+});
 
-  console.log("DB Connected");
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
 
-  app.listen(port, () => {
-    console.log(`Server running on ${port}`);
-  });
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
 });
