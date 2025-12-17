821ce9a6d6d39385c23070907e2b5d9c53dd9f82
Darkryza <82858316+Darkryza@users.noreply.github.com>
2025-12-17 17:38:30 +0800

    update

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,21 +1,49 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import "./RegisterPage.css";
 
 const RegisterPage = () => {
+  const [register, setRegister] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setRegister((prev) => ({
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = () => {};
+
   return (
-    <div>
+    <div className="register-page">
       <form className="register-container">
         <label htmlFor="username">Username</label>
-        <input type="text" placeholder="Create username" name="username" />
+        <input
+          type="text"
+          placeholder="Create username"
+          name="username"
+          onChange={handleChange}
+        />
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" placeholder="Create password" />
-        <label htmlFor="confirm-password">Confirm Password</label>
         <input
           type="password"
-          name="confirm-password"
+          name="password"
+          placeholder="Create password"
+          onChange={handleChange}
+        />
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input
+          type="password"
+          name="confirmPassword"
           placeholder="Confirm your password"
+          onChange={handleChange}
         />
         <button>Submit</button>
-        <Link to="/">Create account</Link>
+        <Link to="/">Back to login</Link>
       </form>
     </div>
   );
