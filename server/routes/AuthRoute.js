da8b5469316e52ddfa4a473f9e3e5d8b4cca92cc
Darkryza <82858316+Darkryza@users.noreply.github.com>
2026-01-08 20:26:32 +0800

    Refactor authentication routes to use bcrypt for password hashing and comparison

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -1,32 +1,44 @@
 import express from "express";
 import conn from "../utils/db.js";
 import jwt from "jsonwebtoken";
+import bycrypt from "bcrypt";
 
 const router = express.Router();
 
 router.post("/login", (req, res) => {
   const { username, password } = req.body;
-  const query = "SELECT * FROM USERS WHERE username=? AND password=?";
-  conn.query(query, [username, password], (err, result) => {
+  const query = "SELECT * FROM users WHERE username=?";
+  conn.query(query, [username], (err, result) => {
     if (err) {
       console.log("DB error ", err);
       return res.json({ status: false, message: "DB error" });
     }
     if (result.length > 0) {
-      const id = result[0].id;
-      const username = result[0].username;
-      const token = jwt.sign(
-        {
-          id: id,
-          username: username,
-        },
-        process.env.JWT_SECRETKEY,
-        { expiresIn: process.env.JWT_EXPIRES }
-      );
-      return res.json({
-        status: true,
-        message: "Login successful",
-        token: token,
+      const user = result[0];
+      bycrypt.compare(password, user.password, (err, isMatch) => {
+        if (err) {
+          console.log(err);
+          return res.json({ status: false, message: "Error on server side" });
+        }
+        if (!isMatch) {
+          return res.json({
+            status: false,
+            message: "Username or Password is incorrect",
+          });
+        }
+        const token = jwt.sign(
+          {
+            id: user.id,
+            username: user.username,
+          },
+          process.env.JWT_SECRETKEY,
+          { expiresIn: process.env.JWT_EXPIRES }
+        );
+        return res.json({
+          status: true,
+          message: "Login successful",
+          token: token,
+        });
       });
     } else {
       return res
@@ -39,16 +51,22 @@ router.post("/login", (req, res) => {
 router.post("/register", (req, res) => {
   const { username, password } = req.body;
   const query = "INSERT INTO users (username,password) VALUES (?,?)";
-  conn.query(query, [username, password], (err, result) => {
+  bycrypt.hash(password, 10, (err, hashed) => {
     if (err) {
-      console.log("DB error", err);
-      return res.json({ message: "DB error" });
+      console.log(err);
+      return res.json({ status: false, message: "Error on server side" });
     }
-    if (result)
-      return res
-        .status(201)
-        .json({ status: true, message: "Register success" });
-    else return res.json({ status: false });
+    conn.query(query, [username, hashed], (err, result) => {
+      if (err) {
+        console.log("DB error ", err);
+        return res.json({ status: false, message: "DB error" });
+      }
+      if (result)
+        return res
+          .status(201)
+          .json({ status: true, message: "Register success" });
+      else return res.json({ status: false });
+    });
   });
 });
 
