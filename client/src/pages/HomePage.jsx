694a5c239d601bcd5af826b67d835ac63e359c2a
Darkryza <82858316+Darkryza@users.noreply.github.com>
2026-01-10 17:45:19 +0800

    update

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import "./HomePage.css";
 
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className="page">
+      <div className="top-summary-quantity">
+        <div className="quantity-service"></div>
+        .spend-service
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
