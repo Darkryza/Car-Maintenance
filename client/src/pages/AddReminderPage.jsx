dcfe00672643db1af66fe43cc05379e79d2aa075
Darkryza <82858316+Darkryza@users.noreply.github.com>
2026-02-04 16:41:39 +0800

    Refactor code structure for improved readability and maintainability

--- client/src/pages/AddReminderPage.jsx
+++ client/src/pages/AddReminderPage.jsx
@@ -1,9 +1,16 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./AddReminderPage.css";
 
 const AddReminderPage = () => {
+  const [values, setValues] = useState({
+    service: "",
+    cost: "",
+    location: "",
+    file: "",
+  });
   return (
     <form className="page addReminderPage">
+      <div className="top"></div>
       <div className="left">
         <h1>New Form</h1>
         <label htmlFor="service">Service</label>
@@ -12,7 +19,7 @@ const AddReminderPage = () => {
         <input type="text" id="cost" />
         <label htmlFor="location">Location</label>
         <input type="text" id="location" />
-        <label htmlFor="Image">Image</label>
+        <label htmlFor="receipt">Image</label>
         <div className="file-input">
           <label htmlFor="receipt" className="select-file-btn">
             {/* {values.file ? values.file.name : "Select File"} */}
@@ -23,7 +30,10 @@ const AddReminderPage = () => {
           <input type="file" id="receipt" name="file" />
         </div>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <label htmlFor="remark">Remark</label>
+        <textarea name="remark" id="remark"></textarea>
+      </div>
       <div className="bottom"></div>
     </form>
   );
