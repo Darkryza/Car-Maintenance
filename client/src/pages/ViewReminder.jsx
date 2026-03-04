7ba8c55c15cd880a88fcd14b6c888e48de541703
Darkryza <82858316+Darkryza@users.noreply.github.com>
2026-03-04 23:44:19 +0800

    update

--- client/src/pages/ViewReminder.jsx
+++ client/src/pages/ViewReminder.jsx
@@ -10,27 +10,36 @@ function ViewReminder() {
   return (
     <div className="page ViewReminder-container">
       <div className="top">
-        <h1>{state.service}</h1>
         <div className="contents">
-          <div className="details">
-            <ul>
-              <li>RM{state.cost}</li>
-              <li>{state.location}</li>
-              <li>{state.status}</li>
-              <li>{state.remark}</li>
-            </ul>
+          <div className="title">
+            <h1>{state.service}</h1>
           </div>
-          <div className="img">
-            <img src="" alt="" />
+          <div className="details">
+            <div className="detail">
+              <ul>
+                <li>{state.cost}</li>
+                <li>{state.location}</li>
+                <li>{state.status}</li>
+                <li>{state.remark}</li>
+              </ul>
+            </div>
+            <div className="img">
+              {state.file && (
+                <img
+                  src={`http://localhost:5484/images/${state.file}`}
+                  alt="receipt"
+                />
+              )}
+            </div>
+            <div className="status">
+              <h2>Status</h2>
+              <div className="btn-done">Done</div>
+              <div className="btn-pending">Pending</div>
+            </div>
           </div>
         </div>
       </div>
       <div className="bottom"></div>
-      {/* <h1>{state.service}</h1>
-      <div>RM{state.cost}</div>
-      <div>{state.location}</div>
-      <div>{state.status}</div>
-      <div>{state.remark}</div> */}
     </div>
   );
 }
