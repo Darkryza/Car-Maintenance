34877f0a78d5e26eb1c486c0f70e3da3b8df5bc8
Darkryza <82858316+Darkryza@users.noreply.github.com>
2026-05-13 11:43:56 +0800

    update

--- client/src/pages/ViewServicesPage.jsx
+++ client/src/pages/ViewServicesPage.jsx
@@ -4,7 +4,19 @@ import { useLocation } from "react-router-dom";
 const ViewServicesPage = () => {
   const { state } = useLocation();
   console.log(state);
-  return <></>;
+  return (
+    <>
+      <div className="view-service-page">
+        <div className="data-services">
+          <ul>
+            <li>services: {state.service}</li>
+            <li>date: {state.date}</li>
+            <li>cost: {state.cost}</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ViewServicesPage;
